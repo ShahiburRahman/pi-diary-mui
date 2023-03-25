fc01792a6dfc910340485d03bc900139fdc37a02
ShahiburRahman <mdshahiburrahmanmiyad@gmail.com>
2023-03-25 17:23:24 +0600

    Add Persistent navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,8 @@
-
-import { Grid, Typography } from '@mui/material';
-import {Box} from '@mui/system';
+import { Root } from "./Components/Root";
 
 
 export default function App() {
   return (
-    <Box
-      sx = {{
-        display: 'flex',
-        justifyContent: 'space-evenly'
-      }}
-    >
-      <Typography sx={{
-        bgcolor: 'warning.main'
-      }}>Hello</Typography>
-      <Grid>
-        <h1>hi</h1>
-      </Grid>
-    </Box>
+    <Root/>
   );
 }
---
+++ src/Components/Root.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Navbar } from './Navbar/Navbar'
+
+export const Root = () => {
+  return (
+    <React.Fragment>
+      <Navbar/>
+    </React.Fragment>
+  )
+}
